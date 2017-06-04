6e2c420b12e4b74c35dcd268e897655ca20cc9c0
instigation <instigation@naver.com>
2017-06-05 04:46:45 +0900

    ReLU customize 할 수 있게 함

--- code/mnist_softmax.py
+++ code/mnist_softmax.py
@@ -3,29 +3,34 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.01)
-    return tf.Variable(initial)
+    initial = tf.truncated_normal(shape, stddev=0.01, dtype=tf.float64)
+    return tf.Variable(initial, dtype=tf.float64)
 
 def bias_variable(shape):
-    initial = tf.constant(0.0, shape=shape)
-    return tf.Variable(initial)
+    initial = tf.constant(0.0, shape=shape, dtype=tf.float64)
+    return tf.Variable(initial, dtype=tf.float64)
 
 def loadData():
   ## load data
   mnist = input_data.read_data_sets("../input/MNIST_data/", one_hot=True)
   return mnist
 
-def zeroLayerSoftmax(mnist,learning_rate=0.5, mini_batch_size=100, epochs=1000):
+def zeroLayerSoftmax(mnist,learning_rate=0.5, mini_batch_size=100, epochs=18000):
   ## setup variables
-  x = tf.placeholder(tf.float32, [None, 784])
-  W1 = weight_variable([784, 100])
-  b1 = bias_variable([100])
-  z1 = tf.matmul(x, W1) + b1
-  a1 = tf.nn.sigmoid(z1)
+  x = tf.placeholder(tf.float64, [None, 784])
+
+  ## Fully Connected Layer
+  W_fc1 = weight_variable([784, 100])
+  b_fc1 = bias_variable([100])
+  c = tf.constant(0, dtype=tf.float64)
+  h_fc1 = tf.maximum((tf.matmul(x, W_fc1) + b_fc1)*1, c)
+
+  ## Softmax Layer
   W = weight_variable([100, 10])
   b = bias_variable([10])
-  y = tf.nn.softmax(tf.matmul(a1, W) + b)
-  y_ = tf.placeholder(tf.float32, [None, 10])
+  y = tf.nn.softmax(tf.matmul(h_fc1, W) + b)
+
+  y_ = tf.placeholder(tf.float64, [None, 10])
   ## determine cost fn
   cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
   ## set learning rate
@@ -44,8 +49,8 @@ def zeroLayerSoftmax(mnist,learning_rate=0.5, mini_batch_size=100, epochs=1000):
 
 ret = []
 mnist = loadData()
-print(zeroLayerSoftmax(mnist))
-for _ in range(5):
+times=10
+for _ in range(times):
   ret.append(zeroLayerSoftmax(mnist))
-print(sum(ret)/5)
+print(sum(ret)/times)
 
