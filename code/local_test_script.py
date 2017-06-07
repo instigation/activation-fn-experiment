2684fcbafd9e3ee60434921aa7f3717b90ab9d91
instigation <instigation@naver.com>
2017-06-07 11:35:38 +0900

    stretched sig 돌림

--- code/local_test_script.py
+++ code/local_test_script.py
@@ -4,7 +4,9 @@ import tensorflow as tf
 if __name__ == "__main__":
   def transSigmoid(down):
     return lambda x : tf.nn.sigmoid(x) - tf.constant(down, dtype=tf.float64)
-  fns = [transSigmoid(0.1*i) for i in range(10)]
-  tags = ["sigmoid"+str(0.1*i) for i in range(10)]
-  expr = ActivationfnExperiment(fns, tags, "transsigmoid", 10)
+  def strSigmoid(ratio):
+    return lambda x : tf.nn.sigmoid(x) * ratio
+  fns = [strSigmoid(1 + 0.2*i) for i in range(10)]
+  tags = ["sigmoid"+str(1 + 0.2*i) for i in range(10)]
+  expr = ActivationfnExperiment(fns, tags, "strsigmoid", 10)
   expr.runandsaveoutput()
